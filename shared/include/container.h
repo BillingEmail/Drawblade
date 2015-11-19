633ba32cbb208fe8d4d5daba41de880672576a18
SaddlePaddle <satyadpatel@knights.ucf.edu>
2015-11-19 09:53:29 -0500

    Cleaned up some code and added comments

--- shared/include/container.h
+++ shared/include/container.h
@@ -17,22 +17,19 @@ typedef struct _SDL_container {
  * Create a new container
  */
 
-Container * New_Container(void);
-
-void Container_Destroy(Container *c);
-
-void Container_EditorRefresh(Container *c);
+Container * New_Container(int SCREEN_WIDTH, int SCREEN_HEIGHT);
+void Container_Destroy(Container *container);
+void Container_Refresh(Container *container);
 
 /*  Functions for making parts of the wrapper */
-SDL_Window * MakeWindow(int width, int height);
-
-SDL_Renderer * MakeRenderer(SDL_Window *window);
+SDL_Window * New_Window(int width, int height);
+SDL_Renderer * New_Renderer(SDL_Window *window);
 
 /*  Functions for updating the camera depending on the situation */
+
 /* Update the camera with the arrow keys */
 void KeyBoardUpdateCamera(SDL_Rect *Camera, const uint8_t *KeyboardState);
-
-/* update the camera using player position */
+/* Update the camera using player position */
 void PlayerUpdateCamera(Player *p, SDL_Rect *Camera);
 
 #endif
--- shared/src/container.c
+++ shared/src/container.c
@@ -4,21 +4,37 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-SDLContainer * New_Container(int width, int height) {
+
+/* 
+   This is to set up the container, which holds all SDL Objects
+   It first mallocs the container, then adds in each object one by one
+   It needs the width and height the screen should be so it can make a window
+   of that size
+*/
+SDLContainer * New_Container(int SCREEN_WIDTH, int SCREEN_HEIGHT) {
 	Container * container = malloc(sizeof(Container));
-	container->window = MakeWindow(width, height);
+	
+	//Uses the New_Window function to make the screen
+	container->window = New_Window(SCREEN_WIDTH, SCREEN_HEIGHT);
+	
+	//Adjusts the position of the camera
 	container->camera = malloc(sizeof(SDL_Rect));
-	container->camera->w = width;
-	container->camera->h = height;
+	container->camera->w = SCREEN_WIDTH;
+	container->camera->h = SCREEN_HEIGHT;
 	container->camera->x = 0;
 	container->camera->y = 0;
-	container->renderer = MakeRenderer(container->window);
+
+	//Uses the New_Renderer function to make the renderer
+	container->renderer = New_Renderer(container->window);
+	//Sets up the keyboard
 	container->keyboardstate = SDL_GetKeyboardState(NULL);
+	
+	//Initializes important SDL functions
 	SDL_Init(SDL_INIT_VIDEO);
 	IMG_Init(IMG_INIT_PNG);
 	return container;
 }
-
+/* Destroys the components of the container and frees the pointers */
 void Container_Destroy(Container * container) {
 	SDL_DestroyWindow(container->window);
 	SDL_DestroyRenderer(container->renderer);
@@ -28,7 +44,10 @@ void Container_Destroy(Container * container) {
 	IMG_Quit();
 }
 
-
+/* 
+   The refresh holds all of the SDL functions that need to be called
+   every frame.  These include delaying each frame, getting keyboard input, etc.
+*/
 void Container_Refresh(Container * container) {
 	SDL_RenderPresent(container->renderer);
 	SDL_Delay(16);
@@ -38,9 +57,9 @@ void Container_Refresh(Container * container) {
 }
 
 /* Makes the screen and checks if it is made correctly */
-SDL_Window * MakeWindow(int width, int height) {
+SDL_Window * New_Window(int width, int height) {
 	SDL_Window *ret = SDL_CreateWindow(
-	"LevelEditor", 
+	"The Window", 
 	SDL_WINDOWPOS_CENTERED, 
 	SDL_WINDOWPOS_CENTERED, 
 	width, 
@@ -57,7 +76,7 @@ SDL_Window * MakeWindow(int width, int height) {
 }
 
 /* Makes a renderer */
-SDL_Renderer * MakeRenderer(SDL_Window *window) {
+SDL_Renderer * New_Renderer(SDL_Window *window) {
 	SDL_Renderer *ret = SDL_CreateRenderer(
 	window,
 	-1,
@@ -72,26 +91,26 @@ SDL_Renderer * MakeRenderer(SDL_Window *window) {
 }
 
 /* Updates camera depending on arrow keys */
-void KeyBoardUpdateCamera(SDL_Rect *Camera, const uint8_t *KeyboardState) {
-	if (KeyboardState[SDL_SCANCODE_UP]) {
-		Camera->y -= 5;
+void KeyBoardUpdateCamera(Container *container) {
+	if (container->keyboardstate[SDL_SCANCODE_UP]) {
+		container->camera->y -= 5;
 	}
-	if (KeyboardState[SDL_SCANCODE_DOWN]) {
-		Camera->y += 5;
+	if (container->keyboardstate[SDL_SCANCODE_DOWN]) {
+		container->camera->y += 5;
 	}
-	if (KeyboardState[SDL_SCANCODE_RIGHT]) {
-		Camera->x += 5;
+	if (container->keyboardState[SDL_SCANCODE_RIGHT]) {
+		container->camera->x += 5;
 	}
-	if (KeyboardState[SDL_SCANCODE_LEFT]) {
-		Camera->x -= 5;
+	if (container->keyboardstate[SDL_SCANCODE_LEFT]) {
+		container->camera->x -= 5;
 	}
 
 }
 
 /* Updates camera depending on player's coordinates */
-void PlayerUpdateCamera(SDL_Rect *Camera, CharacterType *ct, int instance_index) {
-	Camera->x = ct->object_type->instances[instance_index].dstrect.x - Camera->w / 2;
-	Camera->y = ct->object_type->instances[instance_index].dstrect.y - Camera->h / 2;
+void PlayerUpdateCamera(Container *container, CharacterType *ct, int instance_index) {
+	container->camera->x = ct->object_type->instances[instance_index].dstrect.x - Camera->w / 2;
+	container->camera->y = ct->object_type->instances[instance_index].dstrect.y - Camera->h / 2;
 }
 
 
