052e9d0945b390bc2f6685a79750685a7b21dee7
kylercn <kylercndn@gmail.com>
2015-11-21 17:09:42 -0500

    Removed startscreen

--- MainGame/include/startscreen.h
+++
@@ -1,9 +0,0 @@
-#ifndef STARTSCREEN_H
-#define STARTSCREEN_H
-
-#include "../../shared/include/container.h"
-
-//Used to display start screen and wait for user to click mouse, when clicked will break
-void ShowStartScreen(Container *container);
-
-#endif
