c1932b9af50e1b72bf8852abe13b4b96bc978628
shintoo <seanprapp@gmail.com>
2015-11-22 13:57:08 -0500

    Main Menu functionality

--- MainGame/src/main.c
+++ MainGame/src/main.c
@@ -1,21 +1,25 @@
 //#include "../include/startscreen.h"
 #include "../include/game.h"
 #include "../../shared/include/container.h"
+#include "../include/mainmenu.h"
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
 int main(void) {
-	Container *contianer;
+	Container *container;
 	Game *game;
+	MainMenu *StartScreen;
 
 	container = New_Container(SCREEN_WIDTH, SCREEN_HEIGHT);
-	game = New_Game(container);
-	ShowStartScreen(container);
+	StartScreen = New_MainMenu(container);
 
-	Game_Run(game);
+	if (MainMenu_Show(StartScreen, container) == START) {
+		game = New_Game(container);
+		Game_Run(game);
+		Game_Close(game);
+	}
 
-	Game_Close(game);	
 	Container_Destroy(container);
 
 	return 0;
